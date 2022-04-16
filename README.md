ce8f0cf1c03581459ea3bddebd603117a4da31d0
Dandandooo <batkodanny@gmail.com>
2022-04-16 18:36:40 -0400

    if you want to have a gander

--- README.md
+++ README.md
@@ -1,15 +1,19 @@
 # News-Comparator
+<img src="https://img.shields.io/badge/Contributors-4-orange?logo=github" alt="what" >
+<img src="https://img.shields.io/badge/Minecraft References-∞-green?logo=mojang studios" alt="what">
+<img src="https://img.shields.io/badge/Frontend-Flask-d78fb9?logo=flask&logoColor=d78fb9" alt="what">
+<img src="https://img.shields.io/badge/Backend-Python-blue?logo=python&logoColor=yellow" alt="what">
+
+\
+<img src="https://static.wikia.nocookie.net/minecraft_gamepedia/images/e/e2/Redstone_Comparator_%28S%29_JE4.png" width="50%" height="50%" alt="redstone comparator">
 
 [//]: # ( About: News-Comparator is a tool to compare news articles. )
 [//]: # ( Language: English )
 [//]: # ( License: MIT )
 [//]: # ( Repository: )
-HackTJ 2022 Project
+**HackTJ 2022 Project**
 
-
-
-Written by Daniel Philipov, Bradley Cao, Katie Merrill, and Mina Warner
-
+*Written by Daniel Philipov, Bradley Cao, Katie Merrill, and Mina Warner*
 
 # Its Mission
 To compare the similarities and differences of user-chosen news articles.
--- comparator.py
+++ comparator.py
@@ -3,6 +3,11 @@
 
 import nltk
 import json
+import spacy
+import sys
+
+
+stopwords = set(nltk.corpus.stopwords.words('english'))
 
 def manage_url(url, headline=None, author=None, date_publish=None, publication=None, category=None,
                  source_domain=None, article=None, description=None, keyword=None, **kwargs):
@@ -13,30 +18,36 @@ def manage_url(url, headline=None, author=None, date_publish=None, publication=N
     print(f'{publication=}')
     print(f'{category=}')
     print(f'{description=}')
-    print('Getting Words...')
+    # print('Getting Words...')
     words = nltk.word_tokenize(article)
-    print(words)
-    print('Getting Sentences...')
+    # print(words)
+    # print('Getting Sentences...')
     sents = nltk.sent_tokenize(article)
-    print(sents)
-    print('Getting Contexts...')
+    # print(sents)
+    # print('Getting Contexts...')
     content = nltk.text.Text(words)
-    process_keywords(keyword, content)
-
-
+    # process_keywords(keyword, content)
 
 def process_keywords(keywords, text):
     for keyword in keywords:
         print(f'\x1b[31m{keyword=}\x1b[0m')
-        text.common_contexts(keyword)
+        text.common_contexts([keyword])
         print('\x1b[34msimilar\x1b[0m')
         text.similar(keyword)
         print('\x1b[33mconcordance\x1b[0m')
         text.concordance(keyword)
 
+def find_sents(sents, word):
+    for sent in sents:
+        if word in sent:
+            yield sent
+
+def cosine_similarity(text1, text2) -> float:
+    return nltk.cluster.util.cosine_distance(text1, text2)
 
 if __name__ == '__main__':
     sites = json.load(open('sites.json', 'r'))
 
+    common_keywords = set().union(*[set(data['keyword']) for data in sites.values()])
     for site, data in sites.items():
         manage_url(site, **data)
