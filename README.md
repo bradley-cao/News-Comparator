41aee1bcbb714b461e4ae3639b09ded84262950a
Dandandooo <batkodanny@gmail.com>
2022-04-16 17:20:56 -0400

    katie needs it again

--- README.md
+++ README.md
@@ -1,7 +1,14 @@
 # News-Comparator
+
+[//]: # ( About: News-Comparator is a tool to compare news articles. )
+[//]: # ( Language: English )
+[//]: # ( License: MIT )
+[//]: # ( Repository: )
 HackTJ 2022 Project
 
-Authors: Daniel Philipov, Bradley Cao, Katie Merrill, Mina Warner
+
+
+Written by Daniel Philipov, Bradley Cao, Katie Merrill, and Mina Warner
 
 
 # Its Mission
--- comparator.py
+++ comparator.py
@@ -2,40 +2,41 @@
 # Date: 04/16/2022
 
 import nltk
-import pickle
 import json
 
-def manage_url(url, headline=None, author=None, date_published=None, publisher=None, category=None,
+def manage_url(url, headline=None, author=None, date_publish=None, publication=None, category=None,
                  source_domain=None, article=None, description=None, keyword=None, **kwargs):
     print(f'{url=}')
     print(f'{headline=}')
     print(f'{author=}')
-    print(f'{date_published=}')
-    print(f'{publisher=}')
+    print(f'{date_publish=}')
+    print(f'{publication=}')
     print(f'{category=}')
-    print(f'{source_domain=}')
     print(f'{description=}')
-    content = nltk.text.Text(article)
     print('Getting Words...')
     words = nltk.word_tokenize(article)
+    print(words)
     print('Getting Sentences...')
     sents = nltk.sent_tokenize(article)
+    print(sents)
     print('Getting Contexts...')
+    content = nltk.text.Text(words)
     process_keywords(keyword, content)
 
 
 
 def process_keywords(keywords, text):
     for keyword in keywords:
-        print(f'{keyword=}')
+        print(f'\x1b[31m{keyword=}\x1b[0m')
         text.common_contexts(keyword)
-        print('similar')
+        print('\x1b[34msimilar\x1b[0m')
         text.similar(keyword)
-        print('concordance')
+        print('\x1b[33mconcordance\x1b[0m')
         text.concordance(keyword)
 
 
 if __name__ == '__main__':
     sites = json.load(open('sites.json', 'r'))
-    for site, data in sites:
+
+    for site, data in sites.items():
         manage_url(site, **data)
