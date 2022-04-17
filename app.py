65db70a6ba519301d5eb6e83ce9b56fceb383f38
Mina Warner <warnermina@gmail.com>
2022-04-16 21:03:40 -0400

    small tweaks

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect
+import scrapeurl, comparator
 
-app = Flask(__name__)
+app = Flask('app')
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+url1 = ''
+url2 = ''
+
+@app.route('/', methods=["GET", "POST"])
+def index():
+  if request.method == "POST":
+    form_name = request.form.get("name","")
+    return render_template("final.html", text = form_name)
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+  else: 
+    return render_template("index.html")
+
 
 
 if __name__ == '__main__':
